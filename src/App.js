fe73a6a9b2853d494954956833b0620ab4cfc09f
zerow41 <dudtj0410@naver.com>
2021-09-29 18:08:23 +0900

    2021-09-30 (ch.4-5)

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import ScrollBox from './ScrollBox';
 //import MyComponent from './MyComponent';
 //import Counter from './Counter';
-import Say from './Say';
+//import Say from './Say';
+//import EventPractice from './EventPractice';
+//import ValidationSample from './ValidationSample';
 
-const App = () => {
+class App extends Component {
+  render() {
+    return (
+      //<ValidationSample />
+      <div>
+        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
+        <button onClick={() => this.scrollBox.scrollToBottom()}>
+          맨 밑으로
+        </button>
+      </div>
+    );
+  }
+}
+
+/*const App = () => {
   return (
     /*<MyComponent name="React" favoriteNumber={1}>
       리액트
     </MyComponent>*/
     //<Counter />
-    <Say />
-  );
-};
+    //<Say />
+    //<EventPractice />*/
+//  );
+//}
 
 export default App;
---
+++ src/EventPractice.js
@@ -0,0 +1,60 @@
+import React, {useState} from 'react';
+
+const EventPractice = () => {
+    /*const [username, setUsername] = useState('');
+    const [message, setMessage] = useState('');
+    const onChangeUsername = e => setUsername(e.target.value);
+    const onChangeMessage = e => setMessage(e.target.value);
+    const onClick = () => {
+        alert(username + ': ' + message);
+        setUsername('');
+        setMessage('');
+    };*/
+    const [form, setForm] = useState({
+        username: '',
+        message: ''
+    });
+    const { username, message } = form;
+    const onChange = e => {
+        const nextForm = {
+            ...form,
+            [e.target.name]: e.target.value
+        };
+        setForm(nextForm);
+    };
+    const onClick = () => {
+        alert(username + ': ' + message);
+        setForm({
+            username: '',
+            message: ''
+        });
+    };
+    const onKeyPress = e => {
+        if (e.key === 'Enter') {
+            onClick();
+        }
+    };
+    return (
+        <div>
+            <h1>이벤트 연습</h1>
+            <input 
+                type="text"
+                name="username"
+                placeholder="사용자명"
+                value={username}
+                onChange={onChange} //onChangeUsername
+            />
+            <input 
+                type="text"
+                name="message"
+                placeholder="아무거나 입력해 보세요"
+                value={message}
+                onChange={onChange}
+                onKeyPress={onKeyPress} //onChangeMessage
+            />
+            <button onClick={onClick}>확인</button>
+        </div>
+    );
+};
+
+export default EventPractice;
