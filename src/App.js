c000ec236cc16256805168bba736f9de81b09ccf
zerow41 <dudtj0410@naver.com>
2021-10-05 11:28:25 +0900

    2021-10-05

--- src/App.js
+++ src/App.js
@@ -1,35 +1,33 @@
 import React, {Component} from 'react';
 import './App.css';
-import ScrollBox from './ScrollBox';
-//import MyComponent from './MyComponent';
-//import Counter from './Counter';
-//import Say from './Say';
-//import EventPractice from './EventPractice';
-//import ValidationSample from './ValidationSample';
+import LifeCycleSample from './LifeCycleSample';
+import ErrorBoundary from './ErrorBoundary';
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 16777215).toString(16);
+}
 
 class App extends Component {
+  state = {
+    color: '#000000'
+  }
+
+  handleClick = () => {
+    this.setState({
+      color: getRandomColor()
+    });
+  }
+
   render() {
     return (
-      //<ValidationSample />
       <div>
-        <ScrollBox ref={(ref) => this.scrollBox=ref}/>
-        <button onClick={() => this.scrollBox.scrollToBottom()}>
-          맨 밑으로
-        </button>
+        <button onClick={this.handleClick}>랜덤 색상</button>
+        <ErrorBoundary>
+          <LifeCycleSample color={this.state.color} />
+        </ErrorBoundary>
       </div>
     );
   }
 }
-
-/*const App = () => {
-  return (
-    /*<MyComponent name="React" favoriteNumber={1}>
-      리액트
-    </MyComponent>*/
-    //<Counter />
-    //<Say />
-    //<EventPractice />*/
-//  );
-//}
 
 export default App;
